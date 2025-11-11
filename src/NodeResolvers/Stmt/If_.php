aafa2c1f5b3af46e20666328a04b67fc3e73dec1
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-11-11 11:42:44 -0500

    Update If_.php

--- src/NodeResolvers/Stmt/If_.php
+++ src/NodeResolvers/Stmt/If_.php
@@ -6,6 +6,7 @@ use Laravel\Surveyor\NodeResolvers\AbstractResolver;
 use Laravel\Surveyor\NodeResolvers\Shared\CapturesConditionalChanges;
 use PhpParser\Node;
 use PhpParser\NodeAbstract;
+use Throwable;
 
 class If_ extends AbstractResolver
 {
@@ -13,12 +14,17 @@ class If_ extends AbstractResolver
 
     public function resolve(Node\Stmt\If_ $node)
     {
-        $this->startCapturing($node);
+        try {
+            $this->startCapturing($node);
 
-        // Analyze the condition for type narrowing
-        $this->scope->startConditionAnalysis();
-        $this->from($node->cond);
-        $this->scope->endConditionAnalysis();
+            // Analyze the condition for type narrowing
+            $this->scope->startConditionAnalysis();
+            $this->from($node->cond);
+            $this->scope->endConditionAnalysis();
+        } catch (Throwable $e) {
+            // Make sure to always end the condition analysis no matter what
+            $this->scope->endConditionAnalysis();
+        }
     }
 
     public function resolveForCondition(Node\Stmt\If_ $node)
