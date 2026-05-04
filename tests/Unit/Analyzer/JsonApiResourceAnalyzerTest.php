1bd841fa170f39821d50095f06439f817a2e71ee
joetannenbaum <2702148+joetannenbaum@users.noreply.github.com>
2026-05-04 15:01:40 +0000

    Fix code styling

--- tests/Unit/Analyzer/JsonApiResourceAnalyzerTest.php
+++ tests/Unit/Analyzer/JsonApiResourceAnalyzerTest.php
@@ -6,6 +6,7 @@ use App\Http\Resources\PostResource;
 use Laravel\Surveyor\Analyzer\AnalyzedCache;
 use Laravel\Surveyor\Analyzer\Analyzer;
 use Laravel\Surveyor\Analyzer\ResourceAnalyzer;
+use Laravel\Surveyor\Types\ArrayShapeType;
 use Laravel\Surveyor\Types\ArrayType;
 use Laravel\Surveyor\Types\Entities\JsonApiResourceResponse;
 use Laravel\Surveyor\Types\Entities\ResourceResponse;
@@ -87,7 +88,7 @@ describe('JSON:API ResourceAnalyzer', function () {
         expect($posts->keys())->toEqual(['data']);
 
         $data = $posts->value['data'];
-        expect($data)->toBeInstanceOf(\Laravel\Surveyor\Types\ArrayShapeType::class);
+        expect($data)->toBeInstanceOf(ArrayShapeType::class);
         expect($data->valueType)->toBeInstanceOf(ArrayType::class);
         expect($data->valueType->keys())->toEqual(['id', 'type']);
     });
