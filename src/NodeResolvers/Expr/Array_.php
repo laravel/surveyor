728f469da1854bc02eb569b57fcaffa269ee3d08
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-05-23 10:59:41 -0400

    Update Array_.php

--- src/NodeResolvers/Expr/Array_.php
+++ src/NodeResolvers/Expr/Array_.php
@@ -110,6 +110,7 @@ class Array_ extends AbstractResolver
         $type = $this->from($item->value);
 
         return match (true) {
+            $type instanceof VariableState => $type->type(),
             $type instanceof TypeContract => $type,
             default => Type::mixed(),
         };
